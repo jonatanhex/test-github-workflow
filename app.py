466888fc929a8246802f69337449c949ea6bf0af
vdi-terminal <vdi-terminal@EDU-WSB.local>
2023-05-06 14:31:56 +0200

    add workflow file2

---
+++ app.py
@@ -0,0 +1,9 @@
+"""import"""
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def index():
+    """Module"""
+    return '<h1>Hello WSB! Greetings from Flask!</h1>'
+if __name__ == "__main__":
+    app.run(debug=True)
